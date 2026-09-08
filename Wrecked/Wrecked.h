3ca1f08998772a9162e7b3c63480f8dceed23742
yetzq7 <yetagosto12@icloud.com>
2026-09-07 21:29:36 -0400

    initial structure

---
+++ Wrecked/Wrecked.h
@@ -0,0 +1,10 @@
+#pragma once
+
+class Wrecked 
+{
+public:
+
+	static inline int PlayersNeededToStart = 1;
+	static inline int Port = 7777;
+
+};
---
+++ Wrecked/dllmain.cpp
@@ -0,0 +1,30 @@
+// dllmain.cpp : Defines the entry point for the DLL application.
+#include "framework.h"
+
+
+void WreckedMain
+{
+    AllocConsole();
+    FILE* f;
+    freopen_s(&f, "CONOUT$", "w", stdout);
+    SetConsoleTitleA("Wrecked | Starting..");
+
+}
+
+BOOL APIENTRY DllMain( HMODULE hModule,
+                       DWORD  ul_reason_for_call,
+                       LPVOID lpReserved
+                     )
+{
+    switch (ul_reason_for_call)
+    {
+    case DLL_PROCESS_ATTACH:
+        std::thread(WreckedMain).detach();
+    case DLL_THREAD_ATTACH:
+    case DLL_THREAD_DETACH:
+    case DLL_PROCESS_DETACH:
+        break;
+    }
+    return TRUE;
+}
+
